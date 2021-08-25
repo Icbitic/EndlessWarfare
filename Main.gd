44483fd19db29f9f2dd342b430c1dcaa9f0ffc7c
Icbitic <icbitic@foxmail.com>
2021-08-25 23:35:19 +0800

    Project Structure Optimized
    Changed Mods into PCKLoader
    Divide the Master.tscn

--- Main.gd
+++ Main.gd
@@ -2,6 +2,19 @@ extends Node2D
 var info = {
 	"version": "0.0.1"
 }
+var pcks
+
+signal mod_loaded
 
 func _ready():
-	pass
+	LogRecorder.record("Starting loading mods.")
+	pcks = PckLoader.load_pck("mods")
+	LogRecorder.record(str(pcks.size()) + " mod(s) loaded successfully.")
+	for i in pcks:
+		if is_compatible(i) == false:
+			LogRecorder.record("Warning! " + i.name
+					+ " is not compatible with this version")
+	emit_signal("mod_loaded")
+
+func is_compatible(pck):
+	return Info.info.version.match(pck.version_support)
--- PCKLoader.gd
+++ PCKLoader.gd
@@ -1,28 +1,19 @@
 extends Node2D
 
-var mods = []
-func _ready():
-	LogRecorder.record("Starting loading mods")
-	#Don't use res://mods here
-	dir_contents("mods")
+var pcks = []
+func load_pck(path):
 	
-	LogRecorder.record(str(mods.size()) + " mod(s) loaded successfully")
-	check_mod()
+	#Don't use res:// here
+	search_pck(path)
 	
-	for i in mods:
+	
+	for i in pcks:
 		var node = i.scene.instance()
 		self.add_child(node)
-		
-func check_mod():
-	for i in mods:
-		if is_compatible(i) == false:
-			LogRecorder.record("Warning! " + i.name
-					+ " is not compatible with this version")
-		
-func is_compatible(mod):
-	return Info.info.version.match(mod.version_support)
+	return pcks
 	
-func dir_contents(path, intended = false):
+	
+func search_pck(path, intended = false):
 	var dir = Directory.new()
 	if dir.open(path) == OK:
 		dir.list_dir_begin(true, true)
@@ -30,10 +21,10 @@ func dir_contents(path, intended = false):
 		while file_name != "":
 			if dir.current_is_dir() and !intended:
 				pass
-				dir_contents(path + "//" + file_name, true)
+				search_pck(path + "//" + file_name, true)
 			else:
 				if file_name.match("*.pck"):
-					if dir.get_current_dir() != "mods":
+					if dir.get_current_dir() != path:
 						var file = File.new()
 						var info
 						if (file.open(dir.get_current_dir()
@@ -46,15 +37,15 @@ func dir_contents(path, intended = false):
 								+ "//" + file_name)
 								
 						var scene = load(info.name + ".tscn")
-						var mod = {
+						var pck = {
 							"scene": scene,
 							"name": info.name,
 							"version": info.version,
 							"version_support": info.version_support
 						}
-						mods.append(mod)
+						pcks.append(pck)
 						
 			file_name = dir.get_next()
 	else:
-		LogRecorder.record("Mods folder is missing")
+		LogRecorder.record("pcks folder is missing")
 	dir.list_dir_end()
