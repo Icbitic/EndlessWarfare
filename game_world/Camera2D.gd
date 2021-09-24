6782d1d24c51cafa9b1e9103529147d91883ec9b
Icbitic <icbitic@foxmail.com>
2021-09-24 23:16:10 +0800

    Add in-game console add-on.

--- game_world/Camera2D.gd
+++ game_world/Camera2D.gd
@@ -13,28 +13,29 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * MOVE_SPEED * TEXTURE_SHEET_WIDTH * delta
-	
-	position += velocity
-	
-	
-	if Input.is_action_just_released("zoom_in"):
-		_target_zoom = clamp(zoom.x - ZOOM_SPEED * 0.2, 0.5, 1)
-		_target_zoom = _target_zoom * Vector2(1, 1)
-	if Input.is_action_just_released("zoom_out"):
-		_target_zoom = clamp(zoom.x + ZOOM_SPEED * 0.2, 0.5, 1)
-		_target_zoom = _target_zoom * Vector2(1, 1)
+	if not Console.is_focus:
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * MOVE_SPEED * TEXTURE_SHEET_WIDTH * delta
 		
-	$Tween.interpolate_property(self, "zoom",
-	zoom, _target_zoom, 0.1,
-	Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
-	$Tween.start()
+		position += velocity
+		
+		
+		if Input.is_action_just_released("zoom_in"):
+			_target_zoom = clamp(zoom.x - ZOOM_SPEED * 0.2, 0.25, 0.5)
+			_target_zoom = _target_zoom * Vector2(1, 1)
+		if Input.is_action_just_released("zoom_out"):
+			_target_zoom = clamp(zoom.x + ZOOM_SPEED * 0.2, 0.25, 0.5)
+			_target_zoom = _target_zoom * Vector2(1, 1)
+			
+		$Tween.interpolate_property(self, "zoom",
+		zoom, _target_zoom, 0.1,
+		Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+		$Tween.start()
